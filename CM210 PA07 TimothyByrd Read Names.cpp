bf5fe28b54d4f97ed732196e4d1cade3b8ed2048
TimothyByrd3rd <101755754+TimothyByrd3rd@users.noreply.github.com>
2022-03-16 17:47:45 -0500

    Add files via upload

---
+++ CM210 PA07 TimothyByrd Read Names.cpp
@@ -0,0 +1,61 @@
+/*
+Timothy Byrd
+Fall 2019
+CM210 Computer Science C++
+
+This program in C++ does the following:
+  1) read three student names (first name and last name for each student) and store them
+  2) read four test scores for each student and store them
+  3) process the scores find the total, average
+  4) display the results
+
+  NOTE: a Nested loop
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string firstNames[3];
+  string lastNames[3];
+  int testScores[3][4];//2-dimensional
+  int total[3];
+  float average[3];
+  int i, j;
+  
+  for(i=0;i<3; i++)
+  {
+    cout << "Enter first name of student " << i+1 <<": ";//prompt
+    cin>>firstNames[i];
+    cout<<"Enter last name of student " << i+1 <<": ";
+    cin>> lastNames[i];
+    for(j=0; j<4; j++)
+    {
+      cout<<"Enter Test Score "<<j+1 <<" of " << firstNames[i] << " " << lastNames[i] << ": ";
+      cin>>testScores[i][j];
+
+    }//end of inner loop
+
+  }//end of outer loop
+ for(i=0; i<3; i++)
+ {
+   for(j=0; j<4; j++)
+   {
+     total[i] += testScores[i][j]; //finds the total scores
+   }
+   average[i] = total[i]/4.0;
+ }
+
+for(i=0; i<3; i++)
+  {
+    cout<<firstNames[i] << " " <<lastNames[i] <<"\t";
+    for(j=0; j<4; j++)
+    {
+      cout<<testScores[i][j] <<"\t";
+    }
+    cout<<total[i] << "\t" << average[i] << endl;
+  }
+
+
+}
